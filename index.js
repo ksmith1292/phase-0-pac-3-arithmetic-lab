aa686a7040fff78b9721f8df67a36f0dd75ac93a
Kevin <kevin_smith1292@yahoo.com>
2023-07-29 16:52:33 -0500

    parse lesson

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b) {
+    return a + b
+  }
+function subtract(a,b) {
+    return a - b
+  }
+function divide(a,b) {
+    return a / b
+  }
+function multiply(a,b) {
+    return a * b
+  }
+  let n = (10);
+  function increment(n) {
+    return n += 1;
+  }
+  function decrement(n) {
+    return n -= 1;
+  }
+
+  function makeInt(string) {
+    const parsed = parseInt(string, 10);
+    return parseInt(string, 10);
+     }
+
+function preserveDecimal(string) {
+    const parsed = parseFloat(string,.1);
+    return parseFloat(string, .1);
+     }
